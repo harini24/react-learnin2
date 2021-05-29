c553453df4ce1147b4a963c1660688229d55387b
harini <harinisiva1997@gmail.com>
2021-05-29 11:58:31 +0530

    storing meals in backend and fetching them via http req

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,34 +1,30 @@
+import { useEffect, useState } from 'react';
 import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css'
 import MealItem from './MealItem/MealItem';
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
 const AvailableMeals = props => {
-    const mealsList = DUMMY_MEALS.map(meal => {
+    const [meals, setmeals] = useState([])
+
+    useEffect(() => {
+        fetchMeals()
+    }, [])
+
+    const fetchMeals = async () => {
+        const response = await fetch('https://react-burger-builder-29b01-default-rtdb.firebaseio.com/meals.json')
+        const data = await response.json()
+        const mealsList1 = []
+        for (const meal in data) {
+            const mealInfo = {}
+            console.log(meal)
+            mealInfo['id'] = meal
+            mealInfo['name'] = data[meal].name
+            mealInfo['description'] = data[meal].description
+            mealInfo['price'] = data[meal].price
+            mealsList1.push(mealInfo)
+        }
+        setmeals(mealsList1)
+    }
+    const mealsList = meals.map(meal => {
         return <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price} id={meal.id}></MealItem>
     })
     return <section className={classes.meals}>
